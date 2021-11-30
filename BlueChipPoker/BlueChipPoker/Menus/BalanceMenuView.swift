d4b46ca77fdc0d6673392e29c6cbd5778bb121e9
tjudice <tjudice@umich.edu>
2021-11-30 17:10:19 -0500

    Adding change username and modifying user settings menu screen

--- BlueChipPoker/BlueChipPoker/Menus/BalanceMenuView.swift
+++ BlueChipPoker/BlueChipPoker/Menus/BalanceMenuView.swift
@@ -2,42 +2,67 @@
 //  BalanceMenuView.swift
 //  BlueChipPoker
 //
-//  Created by Ameer Mubarez on 11/24/21.
+//  Created by Ameer Mubarez and Trevor Judice on 11/24/21.
 //
 
 import SwiftUI
 
 struct BalanceMenuView : View{
     @Binding var isPresented: Bool
-    @State private var balance = ""
     @EnvironmentObject var userInfo : UserInfo
+    @State private var balance = ""
+    @State private var new_username = ""
 
     var body: some View{
-        VStack{
-            Text("Enter your new balance:")
-            TextField("", text: $balance)
-                .keyboardType(.numberPad)
-                .padding(10)
-                .background(Color.white)
-                .cornerRadius(20)
-                .shadow(color: .gray, radius: 10)
-                .frame(width: 200)
-                .padding()
-            HStack{
-                Button("Submit"){
+        HStack{
+            VStack{
+                Text("Enter balance to add:")
+                TextField("0", text: $balance)
+                    .keyboardType(.numberPad)
+                    .padding(10)
+                    .background(Color.white)
+                    .cornerRadius(20)
+                    .shadow(color: .gray, radius: 10)
+                    .frame(width: 200)
+                    .padding()
+                Button("Add Balance"){
                     userInfo.balance += Int(balance) ?? 0
-                    //print(userBalance)
-                    isPresented.toggle()
+                    balance = ""
+
                 }
                 .padding()
-                Button("Cancel"){
-                    balance = "0"
-                    isPresented.toggle()
+
+                Text("Enter New Username:")
+                TextField("Enter Username...", text: $new_username)
+                    .keyboardType(.default)
+                    .padding(10)
+                    .background(Color.white)
+                    .cornerRadius(20)
+                    .shadow(color: .gray, radius: 10)
+                    .frame(width: 200)
+                    .padding()
+                Button("Change Username"){
+                    if (new_username != ""){
+                        userInfo.username = new_username
+                    }
+                    new_username = ""
                 }
                 .padding()
+
+            } .frame(width: 400, height: 400)
+                .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+            VStack{
+                Text("Username: " + userInfo.username)
+                Text("Balance: " + String(userInfo.balance))
+                    .padding()
+                Button("Back to menu"){
+                    balance = ""
+                    new_username = ""
+                    isPresented.toggle()
+                }
             }
-        } .frame(width: 300, height: 200)
-            .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+
+        }
     }
 } // body
 
